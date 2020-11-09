9f0ee5285401fc4e64accb85baafd4c7ecb73673
Fusier <47882993+Fusier@users.noreply.github.com>
2020-11-09 14:24:42 +0200

    Remove backend

--- blog-back/api/controllers/homeController.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-exports.get = function(req, res) {
-    res.json('found route!');
-};
--- blog-back/api/routes/homeRoutes.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-module.exports = function(app) {
-  const home = require('../controllers/homeController');
-  app.route('/')
-    .get(home.get);
-};
