d13cb9c9f1d96144aefcd23ef3164b58a3a1c2fd
Ville Kautto <ville.kautto@hotmail.fi>
2020-11-12 13:04:48 +0200

    Add and fill faq page

---
+++ src/app/faq/faq.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-contact",
+  templateUrl: "./faq.component.html",
+  styleUrls: ["./faq.component.css"]
+})
+export class FaqComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
