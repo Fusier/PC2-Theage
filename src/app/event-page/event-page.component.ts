9be940ed686b047162fa961156bb689fd32a3be3
Sorellum <lassi.markkinen@hotmail.com>
2020-11-13 19:15:05 +0200

    Add APIservice to event component.

--- src/app/event-page/event-page.component.ts
+++ src/app/event-page/event-page.component.ts
@@ -1,13 +1,20 @@
 import { Component, OnInit, Output } from '@angular/core';
 import { Event } from "../../types/event";
+import { APIService } from "../API.service";
 
 @Component({
   selector: 'app-event',
   templateUrl: './event-page.component.html',
   styleUrls: ['./event-page.component.css']
 })
-export class EventPageComponent {
+export class EventPageComponent implements OnInit {
   events: Event[];
-  constructor() {}
+  constructor(private api: APIService) {}
 
+  async ngOnInit() {
+    this.api.ListEvents().then(event => {
+      this.events = event.items;
+      console.log(this.events);
+    });
+  }
 }
