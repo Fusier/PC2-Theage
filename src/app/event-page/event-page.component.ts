955992d2cb2722f6769bfce8162489da62e97aba
Sorellum <lassi.markkinen@hotmail.com>
2020-11-13 13:53:10 +0200

    Add EventPageComponent files.

---
+++ src/app/event-page/event-page.component.ts
@@ -0,0 +1,11 @@
+import {Component, OnInit, Output} from '@angular/core';
+
+@Component({
+  selector: 'app-event',
+  templateUrl: './event-page.component.html',
+  styleUrls: ['./event-page.component.css']
+})
+export class EventPageComponent {
+  constructor() {}
+
+}
