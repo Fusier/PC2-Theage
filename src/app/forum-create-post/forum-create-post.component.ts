f7994d937e60c84066e07480e9228e0672f92021
Ville Kautto <ville.kautto@hotmail.fi>
2020-11-23 15:46:36 +0200

    add category selection to post creation page

--- src/app/forum-create-post/forum-create-post.component.ts
+++ src/app/forum-create-post/forum-create-post.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from '../../types/category';
 
 @Component({
   selector: 'app-forum-create-post',
@@ -11,5 +12,38 @@ export class ForumCreatePostComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+
+  categories: Category[] = [{id: 'testid', name: 'testcategory', subcategories: [{
+    id: 'testid', name: 'testsubcategory', posts: [{
+      id: 'testpost', title: 'testtitle', content: 'testcontent', comments: [{
+        id: 'testcomment', content: 'testcontent'
+        }]
+      }]
+  }]}, {id: 'testid', name: 'testcategory', subcategories: [{
+      id: 'testid', name: 'testsubcategory', posts: [{
+        id: 'testpost', title: 'testtitle', content: 'testcontent', comments: [{
+          id: 'testcomment', content: 'testcontent'
+        }]
+      }]
+    }]}, {id: 'testid', name: 'testcategory', subcategories: [{
+      id: 'testid', name: 'testsubcategory', posts: [{
+        id: 'testpost', title: 'testtitle', content: 'testcontent', comments: [{
+          id: 'testcomment', content: 'testcontent'
+        }]
+      }]
+    }]}, {id: 'testid', name: 'testcategory', subcategories: [{
+      id: 'testid', name: 'testsubcategory', posts: [{
+        id: 'testpost', title: 'testtitle', content: 'testcontent', comments: [{
+          id: 'testcomment', content: 'testcontent'
+        }]
+      }]
+    }]}, {id: 'testid', name: 'testcategory', subcategories: [{
+      id: 'testid', name: 'testsubcategory', posts: [{
+        id: 'testpost', title: 'testtitle', content: 'testcontent', comments: [{
+          id: 'testcomment', content: 'testcontent'
+        }]
+      }]
+    }]}];
 
 }
