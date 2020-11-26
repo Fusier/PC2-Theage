ef45823e93755f73372a0305733fb193bee95e69
Fusier <47882993+Fusier@users.noreply.github.com>
2020-11-26 18:16:19 +0200

    QoL changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,6 +27,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { ForumCreatePostComponent } from './forum-create-post/forum-create-post.component';
 import { MatInputModule } from '@angular/material/input';
+import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
 
 
 @NgModule({
@@ -59,7 +60,8 @@ import { MatInputModule } from '@angular/material/input';
     AppRoutingModule,
     MatButtonModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatProgressSpinnerModule,
   ],
   exports: [
   ],
