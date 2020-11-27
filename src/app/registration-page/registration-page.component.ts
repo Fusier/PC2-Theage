ad3d58f480ef51c0a25d9af889378c87509ae6d6
Fusier <47882993+Fusier@users.noreply.github.com>
2020-11-27 17:28:43 +0200

    Create registration-page

---
+++ src/app/registration-page/registration-page.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-registration-page",
+  templateUrl: "./registration-page.component.html",
+  styleUrls: ["./registration-page.component.css"]
+})
+export class RegistrationPageComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+
+}
