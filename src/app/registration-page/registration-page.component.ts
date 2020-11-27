9953a5934a7fd767e66b8b297d28412be4d40861
Fusier <47882993+Fusier@users.noreply.github.com>
2020-11-27 19:38:43 +0200

    Send user data to amazon cognito

--- src/app/registration-page/registration-page.component.ts
+++ src/app/registration-page/registration-page.component.ts
@@ -1,13 +1,41 @@
-import { Component, OnInit } from "@angular/core";
+import {Component, OnInit, ViewChild} from '@angular/core';
+import { Auth } from 'aws-amplify';
 
 @Component({
-  selector: "app-registration-page",
-  templateUrl: "./registration-page.component.html",
-  styleUrls: ["./registration-page.component.css"]
+  selector: 'app-registration-page',
+  templateUrl: './registration-page.component.html',
+  styleUrls: ['./registration-page.component.css']
 })
+
 export class RegistrationPageComponent implements OnInit {
+
+  @ViewChild('username') inputUsername;
+  @ViewChild('email') inputEmail;
+  @ViewChild('password') inputPassword;
+
+
   constructor() {}
 
   ngOnInit() {}
 
+/*  handleClear() {
+    this.inputUsername.nativeElement.value = '';
+    this.inputEmail.nativeElement.value = '';
+    this.inputPassword.nativeElement.value = '';
+  }*/
+
+  async signUp() {
+    try {
+      const { user } = await Auth.signUp({
+        username: this.inputUsername.nativeElement.value,
+        password: this.inputPassword.nativeElement.value,
+        attributes: {
+          email: this.inputEmail.nativeElement.value
+        }
+      });
+      console.log(user);
+    } catch (error) {
+      console.log('error signing up:', error);
+    }
+  }
 }
