98553fc17ac259db57bfb1bdb9af5228f248728b
Sorellum <lassi.markkinen@hotmail.com>
2020-11-30 17:19:48 +0200

    Add login check service.

---
+++ src/app/service/login-service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from "@angular/core";
+import { Router } from "@angular/router";
+import {Auth} from "aws-amplify";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+  constructor(private router: Router) {}
+
+  async checkLogin() {
+    try {
+      (await Auth.currentSession()).getIdToken();
+    } catch (error) {
+      console.log(error);
+      await this.router.navigate(['/start-page']);
+    }
+  }
+}
