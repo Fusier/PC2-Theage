11c3c086cd04318af4e18d7c2c56b30be50e7676
Sorellum <lassi.markkinen@hotmail.com>
2020-11-30 17:24:16 +0200

    Update home page with login service.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,22 +1,24 @@
-import { Component, OnInit } from "@angular/core";
-import { Router } from "@angular/router";
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Auth } from 'aws-amplify';
+import { LoginService } from '../service/login-service';
 
-import { BlogPostService } from "../service/blog-post.service";
-import { BlogPost } from "../models/blog-post";
+import { BlogPostService } from '../service/blog-post.service';
+import { BlogPost } from '../models/blog-post';
 
 @Component({
-  selector: "app-home",
-  templateUrl: "./home.component.html",
-  styleUrls: ["./home.component.css"]
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  loading: boolean = true;
+  loading = true;
   posts: BlogPost[];
 
-  constructor(private postService: BlogPostService, private router: Router) {}
+  constructor(private postService: BlogPostService, private login: LoginService) {}
 
-  ngOnInit() {
-    //this.getElements();
+  async ngOnInit() {
+    this.login.checkLogin();
   }
 
   private getElements(): void {
