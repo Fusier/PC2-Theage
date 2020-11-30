012b8646898f734eabbfe295de39d123264f24f9
Sorellum <lassi.markkinen@hotmail.com>
2020-11-30 17:37:03 +0200

    Add login service usage to rest of components.

--- src/app/faq-page/faq-page.component.ts
+++ src/app/faq-page/faq-page.component.ts
@@ -1,21 +1,24 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
 import faqData from '../../assets/json/faqData.json';
+import { LoginService } from '../service/login-service';
 
 interface Item {
-  title: String;
-  questions: String[];
-  answers: String[];
+  title: string;
+  questions: string[];
+  answers: string[];
 }
 
 @Component({
-  selector: "app-faq-page",
-  templateUrl: "./faq-page.component.html",
-  styleUrls: ["./faq-page.component.css"]
+  selector: 'app-faq-page',
+  templateUrl: './faq-page.component.html',
+  styleUrls: ['./faq-page.component.css']
 })
 export class FaqComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit() {}
+  constructor(private login: LoginService) {}
 
   items: Item[] = faqData;
+
+  ngOnInit() {
+    this.login.checkLogin();
+  }
 }
