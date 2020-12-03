d265a0bfd92779a0fd5b66b90bdfad49b4eacfdf
Fusier <47882993+Fusier@users.noreply.github.com>
2020-12-03 15:57:10 +0200

    Create single event page

---
+++ src/app/single-event-page/single-event-page.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { LoginService } from '../service/login-service';
+import {APIService} from '../API.service';
+import {Comment} from '../../types/comment';
+import {ActivatedRoute} from '@angular/router';
+
+@Component({
+  selector: 'app-single-event-page',
+  templateUrl: './single-event-page.component.html',
+  styleUrls: ['./single-event-page.component.css']
+})
+
+export class SingleEventPageComponent implements OnInit {
+
+  id: string;
+  name: string;
+  venue: string;
+  genre: string;
+  actors: string;
+  description: string;
+  startDate: string;
+  endDate: string;
+  time: string;
+
+  constructor(private api: APIService, private login: LoginService, private route: ActivatedRoute) {
+  }
+
+  async ngOnInit() {
+    this.id = this.route.snapshot.paramMap.get('id');
+    this.api.GetEvent(this.id).then(event => {
+      this.name = event.name;
+      this.venue = event.venue;
+      this.genre = event.genre;
+      this.actors = event.actors;
+      this.description = event.description;
+      this.startDate = event.startDate;
+      this.endDate = event.endDate;
+      this.time = event.time;
+    });
+    await this.login.checkLogin();
+  }
+
+}
